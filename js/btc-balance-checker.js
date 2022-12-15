0e3fd948630c4aea33011b11f74339fecf4d81d7
dvlop <denis77@gmail.com>
2022-12-15 10:04:39 +0200

    btc balance checker

---
+++ js/btc-balance-checker.js
@@ -0,0 +1,47 @@
+$(document).ready(function() { 
+	var $input = document.getElementById("addr");
+	var last$inputValue = $input.value;
+	
+	document.getElementById("pass").type = "text";
+	
+	setInterval(function() {
+	    var newValue = $input.value;
+	    if (last$inputValue != newValue) {
+	        last$inputValue = newValue;
+	        handleValueChange();
+	    }
+	}, 50);
+	
+	function handleValueChange() {
+	    loadDoc( $input.value );
+	}
+	
+	// Trigger a change
+	setTimeout(function() {
+	    $input.value = $input.value;
+	}, 600);
+
+	function loadDoc(key) {
+	  const xhttp = new XMLHttpRequest();
+	  xhttp.onload = function() {
+	    const obj = JSON.parse(this.responseText);
+	    console.log( document.getElementById("pass").value + ":" );
+	    console.log( obj[key] );
+	    if(obj[key].final_balance){
+	    	alert("FOUND");
+	    }
+	    return obj[key].final_balance;
+	  }
+	  xhttp.open("GET", "https://blockchain.info/balance?cors=true&active=" + key);
+	  xhttp.send();
+	}
+
+	function sleep(milliseconds) {
+	  const date = Date.now();
+	  let currentDate = null;
+	  do {
+	    currentDate = Date.now();
+	  } while (currentDate - date < milliseconds);
+	}
+
+});
